541c0b751383de2d9bb4a6cdd97e57b2c3f69abe
Eric Patton <3rix40@gmail.com>
2026-03-13 16:12:07 -0500

    start state generation
    Change-Id: Iacf4246cdcabc863bd68119fd34b0644ac3b12bd

--- src/transform/state.rs
+++ src/transform/state.rs
@@ -1,8 +1,72 @@
+use syn::Ident;
 
+use crate::parse::ScriptData;
 
-fn get_state_code() -> proc_macro2::TokenStream {
+fn get_state_code(state_type: &Ident, scrip_data: &ScriptData) -> proc_macro2::TokenStream {
+    let props = scrip_data.props.iter().map(|prop| {
+        let name = &prop.name;
+        let ty = &prop.ty;
+        quote::quote! {
+            #name: #ty,
+        }
+    });
+
+    let bindable_props = scrip_data.bindable_props.iter().map(|prop| {
+        let name = &prop.name;
+        let ty = &prop.ty;
+        quote::quote! {
+            #name: #ty,
+        }
+    });
+
+    let state_vars = scrip_data.state_vars.iter().map(|var| {
+        let name = &var.name;
+        let ty = &var.ty;
+        quote::quote! {
+            #name: crate::MutateTracker<#ty>,
+        }
+    });
+
+    let derived_vars = scrip_data.derived_vars.iter().map(|var| {
+        let name = &var.name;
+        let ty = &var.ty;
+        quote::quote! {
+            #name: #ty,
+        }
+    });
+
+    let user_funcs = &scrip_data.state_functions;
+    let init_body = if let Some(init_func) = &scrip_data.init_func {
+        let block = &init_func.block;
+        quote::quote! {
+           #block
+        }
+    } else {
+        quote::quote! {}
+    };
+
     quote::quote! {
-        //
+        pub struct #state_type {
+            #(#props)*
+            #(#bindable_props)*
+
+            #(#state_vars)*
+
+            #(#derived_vars)*
+        }
+
+        impl ComponentState for #state_type {
+            fn init(&mut self) {
+                #init_body
+            }
+
+            // TODO: new func
+
+            // TODO: update-derived
+        }
+
+        impl #state_type {
+            #(#user_funcs)*
+        }
     }
 }
-        
